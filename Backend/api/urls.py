ecffa70c47e216283430240ef4fc31ec613a3939
officialayush23 <officialayushp123@gmail.com>
2026-01-12 03:17:17 +0530

    deleted django backend

--- Backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from . import views 
-
-
-urlpatterns = [
-    path('profile/', views.get_profile, name='get_profile'),
-    path('register/', views.register_user, name='register'),
-    path("chatbot/", views.chatbot, name="chatbot"),
-]
